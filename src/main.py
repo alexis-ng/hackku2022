2116934080c3382398a11058f56e03ad27d0914e
Suhaan Syed <67168522+SuhaanSyed@users.noreply.github.com>
2022-04-10 03:34:18 -0500

    updated folder structure

---
+++ src/main.py
@@ -0,0 +1,24 @@
+from get_server_information import TwilioInformation
+from database import Database
+# get information
+
+
+def main():
+    information = []
+    server_info = TwilioInformation(information)
+    obj = server_info.get_link_info()
+    # database = Database(server_info.get_applicant_status(), obj)
+
+
+
+    pass
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
