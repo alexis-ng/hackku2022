47c62d19e846e6c64f33bb51eb05cd1bc1a6f7c8
Suhaan Syed <67168522+SuhaanSyed@users.noreply.github.com>
2022-04-10 04:37:02 -0500

    minor commit

--- src/main.py
+++ src/main.py
@@ -3,14 +3,9 @@ from database import Database
 # get information
 
 
-def main():
-    information = []
+def info(list):
+    information = list
     server_info = TwilioInformation(information)
     obj = server_info.get_link_info()
     database = Database(server_info.get_applicant_status(), obj)
-    
-
-
-
-if __name__ == "__main__":
-    main()
+    x = database.run()
