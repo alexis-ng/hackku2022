75dc57375c49fdc60d7d3d160f8796209e818c49
Suhaan Syed <67168522+SuhaanSyed@users.noreply.github.com>
2022-04-10 01:28:49 -0500

    fixed major issues

--- get_server_information.py
+++ get_server_information.py
@@ -6,6 +6,16 @@ from job_seeker import Job_Seeker
 from extract_keywords import Extractor
 from job_listing import JobListing
 
+def get_info(url, file_name):
+    doc = requests.get(url, stream=True)
+    with open(file_name, 'wb') as f:
+        f.write(doc.content)
+    sr = Recognize(file_name)
+    return sr.convert_to_text()
+
+def extract_description(desc):
+    extractor = Extractor(desc)
+    return extractor.extract()
 
 class TwilioInformation:
     def __init__(self, information) -> None:
@@ -13,22 +23,9 @@ class TwilioInformation:
         self._is_applicant = information[1]
         self._links = information[2]
 
-        pass
  
     def get_applicant_status(self):
         return self._is_applicant
-
-    def get_info(url, file_name):
-        doc = requests.get(url, stream=True)
-        with open(file_name, 'wb') as f:
-            f.write(doc.content)
-        sr = Recognize(file_name)
-        return sr.convert_to_text()
-
-
-    def extract_description(desc):
-        extractor = Extractor(desc)
-        return extractor.extract()
 
 
     def get_link_info(self):
@@ -36,24 +33,24 @@ class TwilioInformation:
             # create a new applicant
             seeker = Job_Seeker(phone_number=self._phone_number)
             
-            name = self._get_info(self._links["name"])
-            age = int(self._get_info(self._links["age"]))
-            zipcode = int(self._get_info(self._links["zipcode"]))
-            distance = self._get_info(self._links["distance"])
+            name = get_info(self._links["name"], "name")
+            age = int(get_info(self._links["age"], "age"))
+            zipcode = int(get_info(self._links["zipcode"], "zipcode"))
+            distance = get_info(self._links["distance"], "distance")
             try:
                 distance = int(distance)
             except:
                 distance = None
-            description = self._get_info(self._links["description"])
-            keywords = self._extract_description(description)
+            description = get_info(self._links["description"], "description")
+            keywords = extract_description(description)
 
             seeker.set_name(name)
             seeker.set_age(age)
             seeker.set_zipcode(zipcode)
             seeker.set_distance(distance)
-            seeker.set_desciption(description)
+            seeker.set_description(description)
             seeker.set_keywords(keywords)
-            seeker.set_links = self._links[2]
+            seeker.set_links = self._links
             
             return seeker
         
@@ -61,18 +58,18 @@ class TwilioInformation:
             # create a new job
             job = JobListing(phone_number=self._phone_number)
             
-            name = self._get_info(self._links["name"])
-            min_age = int(self._get_info(self._links["min_age"]))
-            zipcode = int(self._get_info(self._links["zipcode"]))
-            description = self._get_info(self._links["description"])
-            keywords = self._extract_description(description)
+            name = get_info(self._links["name"], "name")
+            min_age = int(get_info(self._links["min_age"], "min_age"))
+            zipcode = int(get_info(self._links["zipcode"], "zipcode"))
+            description = get_info(self._links["description"], "description")
+            keywords = extract_description(description)
 
             job.set_name(name)
             job.set_min_age(min_age)
             job.set_zipcode(zipcode)
-            job.set_desciption(description)
+            job.set_description(description)
             job.set_keywords(keywords)
-            job.set_links(self._links[2])
+            job.set_links(self._links)
 
             return job
 
